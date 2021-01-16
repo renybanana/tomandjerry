5e4910df564ed2093046654a1be00e9ec02a1edb
renybanana <68443612+renybanana@users.noreply.github.com>
2021-01-15 17:31:55 -0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,26 +1,36 @@
 
-function preload() {
-    //load the images here
+var tom,tomImage, jerry,jerryImage,tomImage2,jerryImage2;
+function preload(){
+    tomImage= loadImage("images\tomOne.png")
+    tomImage2= loadImage("images\tomFour.png")
+    jerryImage= loadImage("images\jerryTwo.png")
+    jerryImage2= loadImage("images\jerryThree.png")
 }
-
 function setup(){
-    createCanvas(1000,800);
-    //create tom and jerry sprites here
-
+  createCanvas(1200,800);
+  tom = createSprite(400,200,80,20);
+  jerry= createSprite(200,200,50,50);
+  tom.addImage(tomImage);
+  jerry.addImage(jerryImage);
+  jerry.scale = 0.1
+  tom.scale = 0.1
 }
-
-function draw() {
-
-    background(255);
-    //Write condition here to evalute if tom and jerry collide
-
-    drawSprites();
-}
-
-
-function keyPressed(){
-
-  //For moving and changing animation write code here
-
-
+function draw(){
+  background(0);
+  
+  tom.x = World.mouseX;
+  tom.y = World.mouseY;
+  if(tom.x - jerry.x < tom.width/2+jerry.width/2 && 
+    jerry.x-tom.x < tom.width/2 + jerry.width/2 && 
+    tom.y - jerry.y < tom.height /2 + jerry.height/2 && 
+    jerry.y - tom.y < tom.height /2 + jerry.height/2){
+    tom.addImage(tomImage2);
+    jerry.addImage(jerryImage2);
+  
+  }
+  else{
+    tom.addImage(tomImage);
+    jerry.addImage(jerryImage);
+  }
+  drawSprites(); 
 }
